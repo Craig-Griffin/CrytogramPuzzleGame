bbe810752ccd9c630804e16631640f153390679c
Craig Griffin CSM2017 <prb17171@telford-11.cis.strath.ac.uk>
2019-02-25 09:59:50 +0000

    starting working on loading in a quote

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Game {
-    private OneToOneMap<Character, Character> currentMapping;
-    private OneToOneMap<Character, Character> solutionMapping;
+
     private String crytogram;
+    private OneToOneMap<Character, Character> currentMapping;
+
+    private String solution;
+    private OneToOneMap<Character, Character> solutionMapping;
+
+
+    private final String FILE_QUOTES = "quotes.txt";
+
+
 
     public Game(Player player, MappingType type) {
         currentMapping = new OneToOneMap<>();
         solutionMapping = new OneToOneMap<>();
         generateCrypto();
     }
+
+    //For testing
+    public Game(){}
+
 
     public void mapLetter(char cipher, char mapping) {
         if (!Character.isAlphabetic(cipher)) {
@@ -37,6 +56,48 @@ public class Game {
 
     }
 
+
+    public String loadRandomQuote(){
+
+            String line = null;
+            ArrayList<String> quotes = new ArrayList<>();
+
+            try {
+
+                FileReader fileReader = new FileReader(FILE_QUOTES);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    quotes.add(line);
+                }
+
+                bufferedReader.close();
+            } catch (FileNotFoundException ex) {
+                System.out.println("Unable to open file '" + FILE_QUOTES + "'");
+            } catch (IOException ex) {
+                System.out.println("Error reading file '" + FILE_QUOTES + "'");
+
+
+            }
+
+            int random = new Random().nextInt(quotes.size());
+            solution = quotes.get(random);
+
+            return quotes.get(random);
+
+        }
+
+    public String encryptQuote(){
+
+        //Assign a random letter of the alphabet 
+        int random = new Random().nextInt(25);
+
+
+        return crytogram;
+    }
+
+
     public void removeLetter() {
 
     }
@@ -54,7 +115,12 @@ public class Game {
     }
 
     private void generateCrypto() {
+        loadRandomQuote();
 
+    }
+
+    private String getSolution(){
+        return solution;
     }
 
     enum MappingType {
